b2135097c687131a134ef9089c550b2a4e34b5fb
Artem <novokreshenov2004@gmail.com>
2024-01-23 23:20:51 +0300

    d

--- src/units/secondsToMMSS.js
+++ src/units/secondsToMMSS.js
@@ -1,2 +1,3 @@
-import moment from 'moment';
-export default (seconds) => moment.utc(seconds * 1000).format('mm:ss')
+import moment from "moment";
+
+export default (seconds) => moment.utc(seconds * 1000).format("mm:ss");
